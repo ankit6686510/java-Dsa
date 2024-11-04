820e92d2cc81cb0eeddb4062cc26c1cee77b3614
Ankit Jha <ankit6686510@gmail.com>
2024-11-04 22:33:43 +0530

    loop problems

---
+++ Loops/PrimeNumbersUpToN.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrimeNumbersUpToN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int N = scanner.nextInt();
+
+        System.out.println("Prime numbers up to " + N + ":");
+        for (int i = 2; i <= N; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
