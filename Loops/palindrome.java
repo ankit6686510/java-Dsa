5c64284529af8866092ae9cdb60d8df966bbf9ee
Ankit Jha <ankit6686510@gmail.com>
2024-11-04 22:33:13 +0530

    loop problem

--- Loops/palindrome.java
+++ Loops/palindrome.java
@@ -1,3 +1,23 @@
-public class palindrome {
-    
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int originalNum = num;
+        int reversed = 0;
+
+        while (num != 0) {
+            int lastDigit = num % 10;
+            reversed = reversed * 10 + lastDigit;
+            num /= 10;
+        }
+
+        if (reversed == originalNum) {
+            System.out.println(originalNum + " is a palindrome.");
+        } else {
+            System.out.println(originalNum + " is not a palindrome.");
+        }
+    }
 }
