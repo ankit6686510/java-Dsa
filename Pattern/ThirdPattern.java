05c4d9c469561f389a257e5f62397f2bb77eac1a
Ankit Jha <ankit6686510@gmail.com>
2024-11-05 11:59:10 +0530

    loop problems

---
+++ Pattern/ThirdPattern.java
@@ -0,0 +1,23 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class ThirdPattern {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int row = 1;
+        int star = n;
+
+        while (row <= n) {
+            int i = 1;
+            while (i <= star) {    // Print stars for the current row
+                System.out.print("* ");
+                i++;
+            }
+            System.out.println();   // Move to the next line after each row is printed
+            row++;
+            star--;                 // Decrease the number of stars for the next row
+        }
+    }
+}
--- Pattern/sec_pattern.java
+++ Pattern/sec_pattern.java
@@ -1,5 +1,22 @@
 package Pattern;
 
+import java.util.Scanner;
+
 public class sec_pattern {
-    
+    public static void main(String[] args) {
+        Scanner scn  =  new Scanner(System.in);
+        int n =  scn.nextInt();
+        int row= 1;
+        int star = 1;
+        while(row <= n){
+            int i = 1;
+            while(i<= star){
+                System.out.print("* ");
+                i++;
+            }
+            row++;
+            star++;
+            System.out.println();
+        }
+    }
 }
