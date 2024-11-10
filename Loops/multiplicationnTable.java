9260f0fb44d9c931e1afbc89b51422cb11a6e61e
Ankit Jha <71430568+ankit6686510@users.noreply.github.com>
2024-11-10 18:11:01 +0000

    new prob

---
+++ Loops/multiplicationnTable.java
@@ -0,0 +1,7 @@
+public class MultiplicationTable {
+    public void printTable(int n) {
+        for (int i = 1; i <= 10; i++) { // Step 1: Loop from 1 to 10
+            System.out.println(n + " * " + i + " = " + (n * i)); // Step 2: Print table line
+        }
+    }
+}
