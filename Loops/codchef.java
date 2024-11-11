750fa0c3a7f69fa39c19750e71def0e372db2c2f
Ankit Jha <71430568+ankit6686510@users.noreply.github.com>
2024-11-11 18:01:31 +0000

    codechef prob

---
+++ Loops/codchef.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SerejaAndDima {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Number of cards
+        int[] cards = new int[n];
+
+        // Read the cards' values
+        for (int i = 0; i < n; i++) {
+            cards[i] = sc.nextInt();
+        }
+
+        int left = 0, right = n - 1; // Pointers for the two ends
+        int serejaScore = 0, dimaScore = 0; // Players' scores
+        boolean isSerejaTurn = true; // To track whose turn it is
+
+        while (left <= right) { // While there are cards left
+            int pick;
+            if (cards[left] >= cards[right]) {
+                pick = cards[left]; // Pick from left
+                left++; // Move left pointer inward
+            } else {
+                pick = cards[right]; // Pick from right
+                right--; // Move right pointer inward
+            }
+
+            // Update scores based on whose turn it is
+            if (isSerejaTurn) {
+                serejaScore += pick; // Sereja's score
+            } else {
+                dimaScore += pick; // Dima's score
+            }
+            isSerejaTurn = !isSerejaTurn; // Alternate turn
+        }
+
+        System.out.println(serejaScore + " " + dimaScore); // Print final scores
+        sc.close();
+    }
+}
