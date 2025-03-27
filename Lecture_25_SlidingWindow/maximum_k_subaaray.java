4f69907fce5872b93291e0883ce74ac696e34dce
Ankit Jha <ankit6686510@gmail.com>
2025-03-27 21:10:39 +0530

    maximum_k_subaaray

---
+++ Lecture_25_SlidingWindow/maximum_k_subaaray.java
@@ -0,0 +1,25 @@
+public class maximum_k_subaaray {
+    //to find=> k size k uss subarry ka sum jo maximum ho
+    public static void main(String[] args) {
+        int[] arr= {2,1,3,4,5,6,7,8,9,2,3};
+        int k  = 3;
+        System.out.println(maxsum_of_size_k(arr, k));
+       
+    }
+    public static int maxsum_of_size_k(int[] arr,int k){
+        //1st window ka sum
+        int sum = 0;
+        int ans = 0;
+        for(int i = 0; i < k ;i++){
+            sum += arr[i];
+        }
+        ans = sum;
+        for(int i  = k;i <arr.length;i++){
+            sum += arr[i];
+            sum -= arr[i-k];
+            ans = Math.max(ans,sum);
+        }
+        return ans;
+
+    }
+}
