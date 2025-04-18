f7d2280e27f20cd67fdbc190014937609f23f00d
Ankit Jha <ankit6686510@gmail.com>
2025-04-19 00:56:04 +0530

    maximum average in subarray

---
+++ Lecture_25_slidingwindow/maximum_average_subarray.java
@@ -0,0 +1,40 @@
+public class maximum_average_subarray {
+    public static void main(String[] args) {
+        int[] nums = {1,12,-5,-6,50,3};
+        int k = 4;
+        System.out.println(maximumaverage(nums,k));
+
+        
+    }
+    public static double maximumaverage(int[] nums, int k) {
+        int sum = 0;
+        for(int i  = 0; i < k ;i++){
+            sum += nums[i];
+        }
+        int maxsum = sum;
+
+        //start karo window ko
+        int startidx = 0;
+        int endidx = k;
+
+        //work karo --> hamar work hai add and subtarct krna window mai pehlay window s remove hoga ad then add karenge
+        while(endidx < nums.length){
+            sum -= nums[startidx];
+            startidx++;
+
+            sum += nums[endidx];
+            endidx++;
+
+            //ans calculation
+
+            maxsum = Math.max(maxsum ,sum);
+
+        }
+        //ans return kar do hamre case mai ans average nikalna thaa
+
+            return(double)maxsum/k;
+
+
+        
+    }
+}
