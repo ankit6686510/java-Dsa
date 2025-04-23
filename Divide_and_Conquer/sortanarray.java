ef933d272f81514674a71774f46c285f1c50d8bf
Ankit Jha <ankit6686510@gmail.com>
2025-04-23 23:16:26 +0530

    divide and conquer

---
+++ Divide_and_Conquer/sortanarray.java
@@ -0,0 +1,32 @@
+package Divide_and_Conquer;
+
+public class sortanarray {
+    private void quickSort(int[] nums, int low, int high) {
+        if (low < high) {
+            int pi = partition(nums, low, high);
+            quickSort(nums, low, pi - 1);
+            quickSort(nums, pi + 1, high);
+        }
+    }
+
+    private int partition(int[] nums, int low, int high) {
+        int pivot = nums[high];
+        int i = low - 1;
+        
+        for (int j = low; j < high; j++) {
+            if (nums[j] < pivot) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+        
+        swap(nums, i + 1, high);
+        return i + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
