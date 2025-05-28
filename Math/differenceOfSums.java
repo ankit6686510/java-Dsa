d4629a66ee6989545102fe23eff6d092a8540c32
Ankit Jha <ankit6686510@gmail.com>
2025-05-28 21:44:06 +0530

    math  for dsa

---
+++ Math/differenceOfSums.java
@@ -0,0 +1,16 @@
+public class differenceOfSums {
+    public int differenceOfSumss(int n, int m) {
+        int divisibleSum = 0;
+        int nonDivisibleSum = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) {
+                divisibleSum += i;
+            } else {
+                nonDivisibleSum += i;
+            }
+        }
+        
+        return nonDivisibleSum - divisibleSum;
+    }
+}
