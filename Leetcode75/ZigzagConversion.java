70249fad0af9e625dcd6f3b5fc45a406f2063666
Ankit Jha <ankit6686510@gmail.com>
2025-06-01 21:28:10 +0530

    leetcode 6

---
+++ Leetcode75/ZigzagConversion.java
@@ -0,0 +1,37 @@
+public class ZigzagConversion{
+   //leetcode 6
+    public String convert(String s, int numRows) {
+        if(numRows == 1){
+            return s;
+        }
+        StringBuilder sb =  new StringBuilder();//bcz it took o(n) tc
+
+        for(int i = 0; i < numRows ; i++){
+            int idx =  i;
+            int deltaSouth = 2 * (numRows -1 -i);
+            int deltaNorth =  2*i;
+            boolean goingSouth = true;
+
+            while(idx < s.length()){
+                sb.append(s.charAt(idx));
+//agr to aap 0th index pr ho tb deltasouth mai add karo
+                if(i == 0){
+                    idx += deltaSouth;
+                }else if(i == numRows -1){
+                    idx += deltaNorth;
+                }else{
+                    if(goingSouth){
+                        idx += deltaSouth;
+                    }else{
+                        idx += deltaNorth;
+                    }
+                    goingSouth =!goingSouth;
+                }
+            }
+        }
+        return sb.toString();
+
+
+    }
+
+}
