ff58c8ff267babd6f71b7f77b0a4bf878401c88e
Ankit Jha <ankit6686510@gmail.com>
2025-06-04 13:47:37 +0530

    leetcode 8

---
+++ Leetcode75/string_to_integer.java
@@ -0,0 +1,33 @@
+public class string_to_integer {
+    public int myAtoi(String s) {
+        int i = 0, n = s.length();
+        int sign = 1;
+        long result = 0;
+
+        // 1. Ignore leading whitespaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // 2. Check sign
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        // 3. Convert digits to number
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            result = result * 10 + digit;
+
+            // 4. Handle overflow
+            if (sign * result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (sign * result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+            i++;
+        }
+
+        return (int)(sign * result);
+    }
+}
