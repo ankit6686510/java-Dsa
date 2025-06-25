ab9ccdbf68f1594c372f9716b2c321f04de87cf9
Ankit Jha <ankit6686510@gmail.com>
2025-06-25 22:57:36 +0530

    binary tree preorder traversal iterative

---
+++ Tree/Tree/Binary_tree_PREORDER_TRAVERSAL_Iterative.java
@@ -0,0 +1,57 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Binary_tree_PREORDER_TRAVERSAL_Iterative {
+
+    class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+
+        public List<Integer> preorderTraversal(TreeNode root) {
+            List<Integer> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(root);
+
+            while (!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                result.add(node.val);
+
+                // Push right first so left is processed first
+                if (node.right != null) {
+                    stack.push(node.right);
+                }
+                if (node.left != null) {
+                    stack.push(node.left);
+                }
+            }
+
+            return result;
+        }
+    }
+
+}
