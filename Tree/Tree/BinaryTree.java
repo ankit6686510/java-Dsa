26231fccd368c2cb2c1dc53855d0c802ca03dc49
Ankit Jha <ankit6686510@gmail.com>
2025-07-03 22:21:53 +0530

    binary tree construction, maximum ,display,height,preorder,post order, inorder,level order

--- Tree/Tree/BinaryTree.java
+++ Tree/Tree/BinaryTree.java
@@ -1,9 +1,13 @@
 package Tree;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+
 public class BinaryTree {
+
     class Node {
+
         int val;
         Node left;
         Node right;
@@ -55,59 +59,63 @@ public class BinaryTree {
         Display(node.left);
         Display(node.right);
     }
+
     //find in tree
-    public boolean find(int item){
-        return find(this.root , item);
+    public boolean find(int item) {
+        return find(this.root, item);
     }
 
-    private boolean find(Node nn , int item){
-        if(nn == null){
+    private boolean find(Node nn, int item) {
+        if (nn == null) {
             return false;
         }
-        if(nn.val == item){
+        if (nn.val == item) {
             return true;
         }
-        boolean left = find(nn.left , item);
-        boolean right  =  find(nn.right ,item);
+        boolean left = find(nn.left, item);
+        boolean right = find(nn.right, item);
         return left || right;
 
     }
 
     //find maximum in tree
-    public int max(){
+    public int max() {
         return max(this.root);
     }
-    private int max(Node node){
+
+    private int max(Node node) {
 
         //base case
-        if(node == null){
+        if (node == null) {
             return Integer.MIN_VALUE;
         }
-        int left =  max(node.left);
-        int right =  max(node.right);
-        return Math.max(node.val , Math.max(left,right));
+        int left = max(node.left);
+        int right = max(node.right);
+        return Math.max(node.val, Math.max(left, right));
 
     }
 
-    public int ht(){
+    public int ht() {
         return ht(this.root);
     }
-    private int ht(Node node){
-        if(node == null){
+
+    private int ht(Node node) {
+        if (node == null) {
             return -1;
         }
         int left = ht(node.left);
         int right = ht(node.right);
-        return Math.max(left , right) + 1;
+        return Math.max(left, right) + 1;
     }
 
     //traversal
-    public void preOrder(){
+    public void preOrder() {
         preOrder(this.root);
         System.out.println();
     }
-    private void preOrder(Node node){
-        if(node == null){
+
+    private void preOrder(Node node) {
+        if (node == null) {
             return;
         }
         System.out.print(node.val + " ");
@@ -116,12 +124,13 @@ public class BinaryTree {
     }
 
     //postorder
-    public void postOrder(){
+    public void postOrder() {
         postOrder(this.root);
         System.out.println();
     }
-    private void postOrder(Node node){
-        if(node == null){
+
+    private void postOrder(Node node) {
+        if (node == null) {
             return;
         }
         postOrder(node.left);
@@ -130,12 +139,13 @@ public class BinaryTree {
     }
 
     //inorder tarversal
-    public void InOrder(){
+    public void InOrder() {
         InOrder(this.root);
         System.out.println();
     }
-    private void InOrder(Node node){
-        if(node == null){
+
+    private void InOrder(Node node) {
+        if (node == null) {
             return;
         }
         InOrder(node.left);
@@ -144,21 +154,20 @@ public class BinaryTree {
     }
 
     //level order traversal
-    public void levelOrder(){
-        Queue<Node> q =  new LinkedList<>();
+    public void levelOrder() {
+        Queue<Node> q = new LinkedList<>();
         q.add(root);
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             Node rootnode = q.poll(); //remove the root node
             System.out.print(rootnode.val + " ");
-            if(rootnode.left != null){
+            if (rootnode.left != null) {
                 q.add(rootnode.left);
             }
-            if(rootnode.right != null){
+            if (rootnode.right != null) {
                 q.add(rootnode.right);
             }
         }
         System.out.println();
     }
-    
 
 }
