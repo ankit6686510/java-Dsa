b03ffe032a93a41c25472f0247f8e8bd8df7d49d
Ankit Jha <71430568+ankit6686510@users.noreply.github.com>
2025-07-23 18:14:01 +0000

    using sort , set, and map

--- Lecture_59_Hashmap/Intersection_of_two_array.java
+++ Lecture_59_Hashmap/Intersection_of_two_array.java
@@ -34,3 +34,62 @@ public class Intersection_of_two_array {
 
     }
 }
+
+//using hashset or set
+/*
+ public int[] intersection(int[] nums1, int[] nums2) {
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int n : nums1) {
+            set1.add(n);
+        }
+        for (int n : nums2) {
+            set2.add(n);
+        }
+
+        set1.retainAll(set2);
+
+        int[] res = new int[set1.size()];
+
+        int i = 0;
+        for (int n : set1) {
+            res[i] = n;
+            i++;
+
+        }
+        return res;
+
+    }
+ */
+//using map
+
+ /*
+  *class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int n : nums1) {
+            map.put(n, 1);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int n : nums2) {
+            if (map.containsKey(n) && map.get(n) == 1) {
+                map.put(n, 0); // mark as already added
+                ans.add(n);
+            }
+        }
+
+        int[] res = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
+
+        return res;
+    }
+}
+  */
+
