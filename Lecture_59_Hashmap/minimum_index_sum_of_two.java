def1b0d9c83e64d54d16b4809db1b12ac07e5918
Ankit Jha <71430568+ankit6686510@users.noreply.github.com>
2025-07-29 17:52:07 +0000

    minium index sum of two list leetcode 599

---
+++ Lecture_59_Hashmap/minimum_index_sum_of_two.java
@@ -0,0 +1,34 @@
+package Lecture_59_Hashmap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class minimum_index_sum_of_two {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<>();
+        List<String> result = new ArrayList<>();
+        int minIndexSum = Integer.MAX_VALUE;
+
+        // store list1 strings with their indices
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
+
+        // check in list2
+        for (int j = 0; j < list2.length; j++) {
+            String word = list2[j];
+            if (map.containsKey(word)) {
+                int sum = j + map.get(word);
+                if (sum < minIndexSum) {
+                    result.clear(); // new smaller sum found
+                    result.add(word);
+                    minIndexSum = sum;
+                } else if (sum == minIndexSum) {
+                    result.add(word); // same min sum → add to list
+                }
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+}
