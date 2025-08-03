93963b67799e7f552cefe3fe0a7d9a60a56cbef7
Ankit Jha <71430568+ankit6686510@users.noreply.github.com>
2025-08-03 17:59:50 +0000

    course schedule leetcode 207

---
+++ Graph/Course_Schedule.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course_Schedule {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++)
+            graph.add(new ArrayList<>());
+        for (int[] pre : prerequisites)
+            graph.get(pre[1]).add(pre[0]);
+
+        int[] visited = new int[numCourses]; // 0 = unvisited, 1 = visiting, 2 = visited
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i, graph, visited))
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean dfs(int course, List<List<Integer>> graph, int[] visited) {
+        if (visited[course] == 1)
+            return false;
+        if (visited[course] == 2)
+            return true;
+
+        visited[course] = 1;
+        for (int next : graph.get(course)) {
+            if (!dfs(next, graph, visited))
+                return false;
+        }
+
+        visited[course] = 2;
+        return true;
+    }
+}
