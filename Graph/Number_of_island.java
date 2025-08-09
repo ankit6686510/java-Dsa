6d4f770ccafb67b050f420c92bab4f3d91708582
Ankit Jha <71430568+ankit6686510@users.noreply.github.com>
2025-08-09 16:59:42 +0000

    number of island

---
+++ Graph/Number_of_island.java
@@ -0,0 +1,24 @@
+public class Number_of_island {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int r, int c) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] != '1')
+            return;
+        grid[r][c] = '0'; // Mark visited
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+}
